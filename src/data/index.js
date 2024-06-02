9ff0167f8811d75647c21f188af30f7720f48631
packsonlyfree <packsonlyfree@gmail.com>
2024-06-02 08:19:46 -0300

    Added new pack

--- src/data/index.js
+++ src/data/index.js
@@ -1,11 +1,17 @@
 import Brasiloirinha from "./Brasiloirinha";
 import CatarinaPaolino from "./Catarina Paolino";
 import CibellyFerreira from "./Cibelly Ferreira";
+import LeyaGornaya from "./Leya Gornaya";
+import SophieRain from "./Sophie Rain";
+import WaifuMiia from "./Waifu Miia";
 
 const data = [
   CibellyFerreira,
   Brasiloirinha,
-  CatarinaPaolino
+  CatarinaPaolino,
+  WaifuMiia,
+  SophieRain,
+  LeyaGornaya
 ];
 
 export default data;
